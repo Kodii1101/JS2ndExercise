d9695416a03845b8bc04e85090a1beb3c6685b71
Jonnell Lazaro J. Bingat <jlbversusxiii@gmail.com>
2024-03-02 05:07:45 +0800

    Another Javascript exercise!

---
+++ main.js
@@ -0,0 +1,36 @@
+/* var myArray =[["Jay", 24], ["hey", 84]];
+var removedArray = myArray.pop();
+
+myArray.push(["jOHNwICK", 35]);
+myArray.unshift(["jOHnWeak", 325])
+
+console.log(removedArray);
+console.log(myArray);
+ */
+
+/* function taxThis(a, b) {
+    const c = 0.12;
+    var ac = a * c;
+    var bc = b * c;
+    var ab = a + b;
+
+    return ac + " " + bc + " " + ab;
+}
+
+console.log(taxThis(3500, 4500));
+
+var testThis = {
+    x: "x ni bhai",
+    y: "y ni bhai",
+    z: "z ni bhai",
+};
+
+console.log(testThis.x); */
+
+/* function xyz(shiwq) {
+    dasdas;
+    dasdasdsa;      
+
+} */
+
+// IM DONE! 3:26:42 thank you Vanzkinz and freeCodeCamp! https://www.youtube.com/watch?v=PkZNo7MFNFg&ab_channel=freeCodeCamp.org
