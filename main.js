9ccc3402499ec1eac59e73e0ce359080ee9b4afc
Jonnell Lazaro J. Bingat <jlbversusxiii@gmail.com>
2024-03-05 03:41:36 +0800

    I answered Mr. Yabes's challenge problem... hopefully its correct.

--- main.js
+++ main.js
@@ -1,36 +1,60 @@
-/* var myArray =[["Jay", 24], ["hey", 84]];
-var removedArray = myArray.pop();
+//challenge question from Mr. Charlie Yabes
+/* Help me to answer. 😭
+As a future athlete you just started your practice for an upcoming event. Given that on the first day you run x miles, and by the event you must be able to run y miles.
+Calculate the number of days required for you to finally reach the required distance for the event, if you increases your distance each day by 10% from the previous day.
+Print one integer representing the number of days to reach the required distance. */
 
-myArray.push(["jOHNwICK", 35]);
-myArray.unshift(["jOHnWeak", 325])
+function checkThisss(startDayKM, eventRequiredKM) {
+    let x = startDayKM; //5
+    let y = eventRequiredKM; //20
+    const iR = 0.1;
+    let daysFinished = 0;
+    let kmFinished = 0;
+    let kmExcess = 0;
+    let currentValue = 0;
 
-console.log(removedArray);
-console.log(myArray);
- */
+    if (x >= y) {
+        return "You already ran what is required. You pass!";
+    } else {
+        if (x <= 0) {
+            return "You haven't moved an inch!";
+        } else {
+            if (y <= 0) {
+                return "How much running do you need? please specify";
+            } else {
+                while (kmFinished < y) {
+                    kmFinished = kmFinished + x;
+                    currentValue = x;
+                    x = x + x * iR;
+                    daysFinished++;
+                    kmExcess = kmFinished - y;
+                }
+                /* console.log(x);
+                console.log(kmFinished);
+                console.log(currentValue);
+                console.log(kmExcess);
+                for (j = 0; j <= 5; j++) {
+                    console.log((x = x + x * iR));
+                } */
+                if (kmExcess >= 7) {
+                    daysFinished--;
+                    kmExcess = y - (kmFinished - currentValue);
+                    kmFinished = kmFinished - currentValue;
+                    return `You will finish in approx ${daysFinished} day/s and run ${kmFinished.toPrecision(
+                        4
+                    )} km/s with a short of ${kmExcess.toPrecision(
+                        3
+                    )} kilometer/s`;
+                } else {
+                    return `You will finish in approx ${daysFinished} day/s and run ${kmFinished.toPrecision(
+                        4
+                    )} km/s with an excess of ${kmExcess.toPrecision(
+                        3
+                    )} kilometer/s`;
+                }
+            }
+        }
+    }
+} //function end
 
-/* function taxThis(a, b) {
-    const c = 0.12;
-    var ac = a * c;
-    var bc = b * c;
-    var ab = a + b;
-
-    return ac + " " + bc + " " + ab;
-}
-
-console.log(taxThis(3500, 4500));
-
-var testThis = {
-    x: "x ni bhai",
-    y: "y ni bhai",
-    z: "z ni bhai",
-};
-
-console.log(testThis.x); */
-
-/* function xyz(shiwq) {
-    dasdas;
-    dasdasdsa;      
-
-} */
-
-// IM DONE! 3:26:42 thank you Vanzkinz and freeCodeCamp! https://www.youtube.com/watch?v=PkZNo7MFNFg&ab_channel=freeCodeCamp.org
+console.log(checkThisss(10, 30));
